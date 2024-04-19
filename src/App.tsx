9d4c6e6986970772b89e767251d9fd7f72f20446
RezaRayan <rezarayan78@gmail.com>
2024-04-19 13:59:09 +0330

    Fix: RTL MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { TextField } from "@mui/material";
+
 const App = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-4 h-screen">
+    <div className="h-screen flex flex-col justify-center items-center gap-4">
       <h1 className="text-4xl font-extrabold">پروژه جمشید</h1>
       <p className="text-[14px]">
         این پروژه یک اپلیکیشن سرگرمی است که با بازی مافیا شروع به کار خواهد کرد
       </p>
+      <TextField
+        dir="rtl"
+        placeholder="تست راست چین بودن MUI"
+        id="outlined-basic"
+        label="تست راست چین بودن MUI"
+        variant="outlined"
+      />
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,16 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.ts";
 
+import RtlTheme from "./Theme.tsx";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RtlTheme>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </RtlTheme>
     </Provider>
   </React.StrictMode>
 );
