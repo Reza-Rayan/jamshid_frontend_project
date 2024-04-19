fc59ee32f6c736eb2c90bf98f020e54d8d4e0ced
RezaRayan <rezarayan78@gmail.com>
2024-04-19 11:36:24 +0330

    Add: Initial Config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-
 const App = () => {
-  return <div className=" text-center">Jamshid FrontEnd Project</div>;
+  return (
+    <div className="flex flex-col justify-center items-center gap-4 h-screen">
+      <h1 className="text-4xl font-extrabold">پروژه جمشید</h1>
+      <p className="text-[14px]">
+        این پروژه یک اپلیکیشن سرگرمی است که با بازی مافیا شروع به کار خواهد کرد
+      </p>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+const AllRoutes = () => {
+  return (
+    <>
+      <Routes>
+        <Route />
+      </Routes>
+    </>
+  );
+};
+
+export default AllRoutes;
