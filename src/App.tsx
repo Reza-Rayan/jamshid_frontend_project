e7bb407ea5e41b56b1bb6e70535c2781b3f7cdea
RezaRayan <rezarayan78@gmail.com>
2024-04-19 16:53:53 +0330

    Add: Veil component image

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 // Custom Components
 import PhoneInput from "./templates/PhoneInput";
 import CButton from "./templates/CButton";
+import Veil from "./components/Veil";
 
 const App = () => {
   return (
-    <div className="h-screen flex flex-col justify-center items-center gap-4">
-      <h1 className="text-4xl font-extrabold text-white">پروژه جمشید</h1>
-      <p className="text-[14px] text-white">
-        این پروژه یک اپلیکیشن سرگرمی است که با بازی مافیا شروع به کار خواهد کرد
-      </p>
+    <div>
+      <Veil />
       <form className="px-4 gap-10 flex flex-col">
         <PhoneInput label="شماره همراه" fullWidth />
         <CButton fullWidth>ادامه</CButton>
---
+++ src/components/Veil.tsx
@@ -0,0 +1,7 @@
+import VeilImg from "../assets/veil.png";
+
+const Veil = () => {
+  return <img src={VeilImg} height={290} width={290} className="mx-auto" />;
+};
+
+export default Veil;
