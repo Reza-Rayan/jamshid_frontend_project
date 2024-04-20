bb9dd5df80fc4877312925256210bbfdb65e628b
reza-rayan <rezarayan78@gmail.com>
2024-04-21 00:08:57 +0330

    Add: Title Component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,19 @@ import Layout from "../layout/Layout";
 
 // Custom Components
 import SuperEvent from "../components/SuperEvent";
+import Title from "../templates/Title";
+import { Box } from "@mui/material";
 
 const Home = () => {
   return (
     <Layout>
       <SuperEvent />
+    <Box className="my-6">
+      <Title title="ایونت های فعال"
+        description="نزدیک ترین کافه ها به موقعیت مکانی شما"
+        href="#"
+      />
+    </Box>
     </Layout>
   );
 };
