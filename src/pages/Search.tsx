0f422a7fe3528d68c76a63c0679d3a239a4cabbc
RezaRayan <rezarayan78@gmail.com>
2024-04-22 21:02:58 +0330

    Add: Search Page

---
+++ src/pages/Search.tsx
@@ -0,0 +1,12 @@
+//Custom Components
+import SearchBox from "../components/SearchBox";
+
+const Search = () => {
+  return (
+    <div className="py-4">
+      <SearchBox />
+    </div>
+  );
+};
+
+export default Search;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 // Pages
 import Home from "../pages/Home";
+import Search from "../pages/Search";
 import AuthPage from "../pages/auth";
 import Explore from "../pages/Explore";
 import Articles from "../pages/Articles";
@@ -11,6 +12,7 @@ const AllRoutes = () => {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/explore" element={<Explore />} />
         <Route path="/articles" element={<Articles />} />
         <Route path="/auth" element={<AuthPage />} />
